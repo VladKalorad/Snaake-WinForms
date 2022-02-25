d3c292113128e6d7be1157dff57eb34d90bd196f
cherniayskivlad <vladchernav210@gmail.com>
2022-02-25 06:01:38 +0300

    snake

---
+++ SnakeLab20/Snake Lab20/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Snake_Lab20
+{
+    partial class Snake
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(547, 449);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(509, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 449);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "Snake";
+            this.Text = "в";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SnakeLab20/Snake Lab20/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_Lab20
+{
+    public partial class Snake : Form
+    {
+        Point[] p;
+        Point apple;
+        int direction;
+        int len = 5;
+        int count;
+        Point die;
+        public Snake()
+        {
+            InitializeComponent();
+            p = new Point[200];
+            len = 5;
+            direction = 3;
+            for (int i = 0; i < 5; i++)
+            {
+                p[0].X = 100;
+                p[0].Y = 100+i*10;
+            }
+            Random x = new Random();
+            Random y = new Random();
+            apple.X = x.Next(0,50)*10;
+            apple.Y = y.Next(0, 50)*10;
+            Random xd = new Random();
+            Random xy = new Random();
+            die.X = xd.Next(5, 40) * 10;
+            die.Y = xy.Next(5, 40) * 10;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Invalidate();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 198; i >= 0; i--)
+            {
+                p[i+1].X = p[i].X;
+                p[i+1].Y = p[i].Y;
+            }
+            if(direction ==1 )
+            {
+                p[0].X = p[1].X-10;
+                p[0].Y = p[1].Y;
+            }
+            if (direction == 2)
+            {
+                p[0].X = p[1].X + 10;
+                p[0].Y = p[1].Y;
+            }
+            if (direction == 3)
+            {
+                p[0].X = p[1].X;
+                p[0].Y = p[1].Y-10;
+            }
+            if (direction == 4)
+            {
+                p[0].X = p[1].X;
+                p[0].Y = p[1].Y+10;
+            }
+            SolidBrush b = new SolidBrush(Color.Green);
+            for (int i = 0; i < len; i++)
+            {
+                e.Graphics.FillEllipse(b,p[i].X,p[i].Y,10,10);
+
+            }
+            SolidBrush a = new SolidBrush(Color.Red);
+                e.Graphics.FillEllipse(a,apple.X, apple.Y, 10, 10);
+            if(p[0].X == apple.X && p[0].Y == apple.Y)
+            {
+                count++;
+                len++;
+                Random x = new Random();
+                Random y = new Random();
+                apple.X = x.Next(0, 50)*10;
+                apple.Y = y.Next(0, 50) *10;
+                label1.Text = Convert.ToString(count);
+            }
+            SolidBrush c = new SolidBrush(Color.Black);
+            e.Graphics.FillEllipse(c, die.X, die.Y, 10, 10);
+            if (p[0].X == die.X && p[0].Y == die.Y)
+            {
+                count = count -3;
+                len = len - 3;
+                Random xd = new Random();
+                Random xy = new Random();
+                die.X = xd.Next(5, 45) * 10;
+                die.Y = xy.Next(5, 40) * 10;
+                label1.Text = Convert.ToString(count);
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Left)
+            {
+                direction = 1;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                direction = 2;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                direction = 3;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                direction = 4;
+            }
+        }
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
